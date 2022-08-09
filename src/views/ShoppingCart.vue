e1f8a1e44ce0cb857004e0364439890e5908da4a
莫让晗晗 <67039861+Miki-Hunter@users.noreply.github.com>
2022-08-09 09:13:07 +0800

    Update ShoppingCart.vue

--- src/views/ShoppingCart.vue
+++ src/views/ShoppingCart.vue
@@ -133,7 +133,7 @@ export default {
       this.updateShoppingCart({ key: key, prop: "check", val: true });
       // 向后端发起更新购物车的数据库信息请求
       this.$axios
-        .post("/api/user/shoppingCart/updateShoppingCart", {
+        .post("/api/user/shoppingCart/updateShoppingCart/", {
           user_id: this.$store.getters.getUser.user_id,
           product_id: productID,
           num: currentValue
@@ -167,7 +167,7 @@ export default {
     // 向后端发起删除购物车的数据库信息请求
     deleteItem(e, id, productID) {
       this.$axios
-        .post("/api/user/shoppingCart/deleteShoppingCart", {
+        .post("/api/user/shoppingCart/deleteShoppingCart/", {
           user_id: this.$store.getters.getUser.user_id,
           product_id: productID
         })
